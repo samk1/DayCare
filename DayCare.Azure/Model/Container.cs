342cc79a0191a12aee2ae8bff29f6b8730d45cfa
Sam Kidman <samkidman@gmail.com>
2023-11-12 13:18:16 +1100

    inline webapp

---
+++ DayCare.Azure/Model/Container.cs
@@ -0,0 +1,24 @@
+﻿namespace DayCare.Azure.Model
+{
+    using System.Collections.Generic;
+    using HashiCorp.Cdktf.Providers.Azurerm.ContainerApp;
+
+    internal class Container
+    {
+        public static ContainerAppTemplateContainerEnv[] BuildEnvironmentVariables(Dictionary<string, string> environmentVariables)
+        {
+            var env = new List<ContainerAppTemplateContainerEnv>();
+
+            foreach (var environmentVariable in environmentVariables)
+            {
+                env.Add(new ContainerAppTemplateContainerEnv
+                {
+                    Name = environmentVariable.Key,
+                    Value = environmentVariable.Value,
+                });
+            }
+
+            return env.ToArray();
+        }
+    }
+}
--- DayCare.Azure/Stacks/ApplicationStack.cs
+++ DayCare.Azure/Stacks/ApplicationStack.cs
@@ -5,6 +5,8 @@ namespace DayCare.Azure.Stacks
     using DayCare.Azure.Constructs.Data;
     using DayCare.Azure.Model;
     using global::Constructs;
+    using HashiCorp.Cdktf.Providers.Azurerm.ContainerApp;
+    using HashiCorp.Cdktf.Providers.Azurerm.ContainerAppEnvironment;
     using HashiCorp.Cdktf.Providers.Azurerm.DataAzurermMssqlServer;
 
     internal class ApplicationStack : BaseAzureStack
@@ -24,26 +26,88 @@ namespace DayCare.Azure.Stacks
                 ResourceGroupName = resourceGroup.Name,
             });
 
-            var webapp = new WebApp(
-                scope: this,
-                appName: containerAppName,
-                resourceGroup: resourceGroup,
-                spec: new ContainerSpec(
-                    image: containerImage,
-                    containerRegistry: new ContainerRegistry(this),
-                    environmentVariables: new Dictionary<string, string>
+            var containerRegistry = new ContainerRegistry(this);
+
+            var containerAppEnvironment = new ContainerAppEnvironment(
+                this,
+                $"{containerAppName}-container-app-environment",
+                new ContainerAppEnvironmentConfig
+                {
+                    Name = $"{containerAppName}-container-app-environment",
+                    ResourceGroupName = resourceGroup.Name,
+                    Location = resourceGroup.Location,
+                });
+
+            var containerApp = new ContainerApp(this, $"{containerAppName}-container-app", new ContainerAppConfig
+            {
+                Name = $"{containerAppName}-container-app",
+                ResourceGroupName = resourceGroup.Name,
+                ContainerAppEnvironmentId = containerAppEnvironment.Id,
+                RevisionMode = "Multiple",
+                Identity = new ContainerAppIdentity
+                {
+                    Type = "SystemAssigned",
+                },
+                Secret = new[]
+                {
+                    new ContainerAppSecret
                     {
+                        Name = "container-registry-password",
+                        Value = containerRegistry.AdminPassword,
+                    },
+                },
+                Ingress = new ContainerAppIngress
+                {
+                    TargetPort = 80,
+                    ExternalEnabled = true,
+                    Transport = "auto",
+                    AllowInsecureConnections = false,
+                    TrafficWeight = new[]
+                    {
+                        new ContainerAppIngressTrafficWeight
                         {
-                            "ConnectionStrings__DefaultConnection",
-                            Database.ConnectionString(mssqlServer.FullyQualifiedDomainName, Database.DatabaseName(containerAppName))
+                            LatestRevision = true,
+                            Percentage = 100,
                         },
-                        { "ASPNETCORE_ENVIRONMENT", "Development" },
-                    }));
+                    },
+                },
+                Registry = new[]
+                {
+                    new ContainerAppRegistry
+                    {
+                        Server = containerRegistry.LoginServer,
+                        Username = containerRegistry.AdminUsername,
+                        PasswordSecretName = "container-registry-password",
+                    },
+                },
+                Template = new ContainerAppTemplate
+                {
+                    Container = new[]
+                    {
+                        new ContainerAppTemplateContainer
+                        {
+                            Name = $"{containerAppName}-template",
+                            Image = containerImage,
+                            Cpu = 0.5,
+                            Memory = "1Gi",
+                            Env = Container.BuildEnvironmentVariables(
+                                new Dictionary<string, string>
+                                {
+                                    {
+                                        "ConnectionStrings__DefaultConnection",
+                                        Database.ConnectionString(mssqlServer.FullyQualifiedDomainName, Database.DatabaseName(containerAppName))
+                                    },
+                                    { "ASPNETCORE_ENVIRONMENT", "Development" },
+                                }),
+                        },
+                    },
+                },
+            });
 
             _ = new DatabaseAccess(
                 scope: this,
                 mssqlServer: mssqlServer,
-                managedIdentityName: webapp.ContainerAppName,
+                managedIdentityName: containerApp.Name,
                 appName: containerAppName,
                 resourceGroup: resourceGroup,
                 id: "container-app-database-access",
